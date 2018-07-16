6553b5a792e8449db7e3effb07c7b447d97f0772
Bin Chen <chen.6133@osu.edu>
2018-07-16 04:39:33 -0400

    adding view for student/new

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -1,2 +1,11 @@
 class StudentController < ApplicationController
+	# GET /users/new
+  def new
+    @student = Student.new
+  end
+
+	# GET /student
+  def index
+    @students = Student.all
+  end
 end
