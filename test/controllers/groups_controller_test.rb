4e186beb60f2581c5eb5460cb5d14b7fd1d61c5a
Chen Bin <chen.6133@osu.edu>
2018-07-19 14:06:59 -0400

    add controller for each models

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class StudentControllerTest < ActionDispatch::IntegrationTest
+class GroupsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
