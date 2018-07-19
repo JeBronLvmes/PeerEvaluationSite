d73e4980df6c0f934204bd3ea82a1251ddee76bb
Josh Wright <wright.1400@buckeyemail.osu.edu>
2018-07-19 17:38:26 -0400

    Added Student and Professor Login Pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :students
-  get 'login_page/login_general'
-  get 'login_page/login_student'
-  get 'login_page/login_professor'
-  root 'login_page#index'
+  get 'login/index'
+  get 'login/student'
+  get 'login/professor'
+  root 'login#index'
 
 end
