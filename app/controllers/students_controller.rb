e030989f8587964f8b3251340a1f3f0efa4d8614
Chen Bin <chen.6133@osu.edu>
2018-07-19 15:00:32 -0400

    adding view and controller method for students.add

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,20 @@
 class StudentsController < ApplicationController
+	def new
+		@student = Student.new
+	end
+	
+	def create
+		@student = Student.new(article_params)
+ 
+		if @student.save
+			redirect_to @student
+		else
+			render 'new'
+		end
+	end
+	
+	private
+		def student_params
+			params.require(:student).permit(:first_name, :last_name, :dot_number, :email)
+		end
 end
