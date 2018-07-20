4269263390506cbf868b30c583591e73f1269e49
Josh Wright <wright.1400@buckeyemail.osu.edu>
2018-07-19 20:33:57 -0400

    Added new professor sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :students
+  resources :professors
   get 'login/index'
   get 'login/student'
   get 'login/professor'
