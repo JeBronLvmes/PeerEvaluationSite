a147eb9d254750dc335566b912af0f3d9e20f813
Chen Bin <chen.6133@osu.edu>
2018-07-23 04:35:56 -0400

    Fix the turbolink bug
    This bug stop the page from loading js file when clicking links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require rails-ujs
 //= require activestorage
-//= require turbolinks
 //= require_tree .
  function showCourses(button){
     var buttons = document.getElementsByName("course")
--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -4,7 +4,9 @@ app.controller('myCon', function($scope, $http) {
     $scope.showDetail = function (prof_id, course_id) {
         $http.get("/professors/" + prof_id + "/courses/" + course_id)
             .then(function(response) {
-                $scope.res = response.data;
+                $scope.course_name = response.data.dept + ' ' +
+                    response.data.number + ' - ' + response.data.name +
+               ' (section ' + response.data.section + ')';
             });
     };
 });
