71f50c911d58261f1731a417103d636084f7ae36
Houyi Fan <fan.685@osu.edu>
2018-07-24 18:25:31 -0400

    Add new evaluation dropdown item for professor page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :students, path: 'students', controllers: { sessions: "students/sessions",registrations: "students/registrations",passwords: "students/passwords", confirmations: "students/confirmations", unlocks: "students/unlocks",profiles: "students/profiles" }
   devise_for :professors, path: 'professors', controllers: { sessions: "professors/sessions",registrations: "professors/registrations",passwords: "professors/passwords", confirmations: "professors/confirmations", unlocks: "professors/unlocks", }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'professors/:pro_id/professor_forms/new', to: 'professor_forms#new'
   get 'professors/:pro_id/professor_forms/:course_id', to: 'professor_forms#get_forms'
   resources :students do
     resources :courses_student, :courses
