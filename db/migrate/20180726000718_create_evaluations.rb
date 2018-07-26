2d90c65cb83b72402683bfa5f8b89dee077e674a
Josh Wright <wright.1400@buckeyemail.osu.edu>
2018-07-25 22:08:45 -0400

    Changed Students Pages

---
+++ db/migrate/20180726000718_create_evaluations.rb
@@ -0,0 +1,17 @@
+# Created by Bin Chen 7/19/18
+# Modified by Jeb Alawi 7/19/18 - added belongs_to
+class CreateEvaluations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :evaluations do |t|
+      t.string :title
+      t.string :due_date
+      t.string :posted_date
+      t.string :professor_form_info
+      t.integer :professor_form_id
+      t.text :student_form_info
+      t.belongs_to :student
+      t.boolean :isCompleted
+      t.timestamps
+    end
+  end
+end
